f73c85922362a5336924293ec59fed1df537afb4
FreeK <andryushka_99@mail.ru>
2022-02-06 14:22:51 +0300

    Решили задачу в дз №1 про показ всех четных чисел от 1 до N

---
+++ Task004/Program.cs
@@ -0,0 +1,10 @@
+﻿// Показать все четные числа от 1 до N.
+Console.WriteLine("Введите значение N: ");
+string numN = Console.ReadLine();
+int number = int.Parse(numN);
+int min = 2;
+while (min <= number)
+{
+    if (min % 2 == 0) Console.Write(min + " ");
+    min++;
+}
