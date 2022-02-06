0412ca7cc358729bd669f563ac217e7fa00f3027
FreeK <andryushka_99@mail.ru>
2022-02-06 12:55:20 +0300

    Решили задачу в дз №1  про нахождение макс числа

---
+++ Task002/Program.cs
@@ -0,0 +1,15 @@
+﻿//Найти максимальное из трех чисел.
+int numA = new Random().Next(1, 99);
+Console.WriteLine ("Первое число: " + numA);
+int numB = new Random().Next(1, 99);
+Console.WriteLine ("Второе число:" + numB);
+int numC = new Random().Next(1, 99);
+Console.WriteLine ("Третье число:" + numC);
+
+int max = numA;
+if (numA > max) max = numA;
+if (numB > max) max = numB;
+if (numC > max) max = numC;
+
+Console.Write("max = ");
+Console.WriteLine(max);
