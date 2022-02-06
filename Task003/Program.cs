7fe82cd6c7facf15a914767ac853411851ca22b7
FreeK <andryushka_99@mail.ru>
2022-02-06 13:39:50 +0300

    Решили задачу в дз №1 про выяснение четное число или нечетное

---
+++ Task003/Program.cs
@@ -0,0 +1,9 @@
+﻿// Выяснить является ли число чётным.
+Console.WriteLine("Введите любое целое число: ");
+string numberStr = Console.ReadLine();
+double num = int.Parse(numberStr);
+if (num % 2 == 0) Console.WriteLine($"Число {num} является четным");
+else
+{
+    Console.WriteLine($"Число {num} неявляется четным");
+}
