5d5a2271d243cd706187fb38895753642b68a4e7
FreeK <andryushka_99@mail.ru>
2022-02-08 16:19:16 +0300

    сделали домашку по C# про то что явлдяется ли номер дня недели выходным

---
+++ Task008/Program.cs
@@ -0,0 +1,17 @@
+﻿// Дано число обозначающее день недели. Выяснить является номер дня недели выходным.
+int GetRandom()
+{
+    int number = new Random().Next(1, 7);
+    return number;
+}
+int randomaizer = GetRandom();
+Console.WriteLine($"Случайное число: {randomaizer}");
+
+if (randomaizer >= 6)
+    Console.WriteLine($"{randomaizer} выходной день.");
+
+else
+    Console.WriteLine($"{randomaizer} не является выходным.");
+
+//Пн,Вт,Ср,Чт,Пт,Сб,Вс
+// 1  2  3  4  5  6  7
