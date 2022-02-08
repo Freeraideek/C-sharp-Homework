3acde1f9b055a31b7ccfc606f683b41fec5244b0
FreeK <andryushka_99@mail.ru>
2022-02-08 15:12:46 +0300

    сделали домашку по C# про удаление второй цифры из трехначного числа

---
+++ Task006/Program.cs
@@ -0,0 +1,13 @@
+﻿// Удалить вторую цифру трехзначного числа
+int GetRandom()
+{
+    int number = new Random().Next(100, 1000);
+    return number;
+}
+int randomaizer = GetRandom();
+Console.WriteLine($"Случайное число: {randomaizer}");
+
+int resultOne = randomaizer / 100;
+int resultTwo = randomaizer % 10;
+Console.WriteLine($"{resultOne} {resultTwo}");
+
