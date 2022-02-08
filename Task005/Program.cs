661aa9c0e7f2d84525c10d1359d22fbf33a885b1
FreeK <andryushka_99@mail.ru>
2022-02-08 13:33:43 +0300

    сделали домашку по C# про показ второй цифры из трехначного числа

---
+++ Task005/Program.cs
@@ -0,0 +1,10 @@
+﻿// Показать второе цифру трехзначного числа
+int GetRandom()
+{
+    int number = new Random().Next(100, 1000);
+    return number;
+}
+int randomaizer = GetRandom();
+Console.WriteLine($"Случайное число: {randomaizer}");
+int result = randomaizer % 100 / 10;
+Console.WriteLine($"вторая цифра трехначного числаа {result}");
