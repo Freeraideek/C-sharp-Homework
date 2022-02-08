b07c25c8528bb58c68f2be706a3a1f362fb3d6cc
FreeK <andryushka_99@mail.ru>
2022-02-08 15:58:37 +0300

    сделали домашку по C# про наъождение третьей цифры и сообщениие что ее нет

---
+++ Task007/Program.cs
@@ -0,0 +1,18 @@
+﻿// Найти третью цифру числа или сообщить, что её нет.
+int GetRandom()
+{
+    int number = new Random().Next(0, 200);
+    return number;
+}
+int randomaizer = GetRandom();
+Console.WriteLine($"Случайное число: {randomaizer}");
+
+int result = randomaizer % 10;
+if(randomaizer > 99)
+{
+    Console.WriteLine($"{result}");
+}
+else
+{
+    Console.WriteLine("Третьей цифры в числе нет.");
+}
