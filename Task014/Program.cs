1ae78e491b866631d9c911e02860ea6fd027ad2d
FreeK <andryushka_99@mail.ru>
2022-02-17 12:13:00 +0300

    Решение задачи 28

---
+++ Task014/Program.cs
@@ -0,0 +1,20 @@
+﻿// Подсчитать сумму цифр в числе
+
+Console.Write("Введите любое целое число число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+while (a <= 0)
+{
+    a = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите любое целое число число: ");
+}
+
+int sum = 0;
+while (a > 0)
+{
+    sum = sum + a % 10;
+    a = a / 10;
+}
+
+Console.WriteLine($"Сумма цифр числа = {sum}");
+
+
