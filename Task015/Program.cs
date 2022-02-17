3af5ba63ee6c3b98f0cce41d42d0aa8823dba1a0
FreeK <andryushka_99@mail.ru>
2022-02-17 13:22:38 +0300

    Решение задачи 30

---
+++ Task015/Program.cs
@@ -0,0 +1,21 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру.
+
+bool result = true;
+string messageIn = String.Empty;
+double number = 0;
+
+while (number <= 0)
+{
+    Console.Write("Введите число > 0: ");
+    messageIn = Console.ReadLine();
+    result = double.TryParse(messageIn, out number);
+}
+Console.Write($"Кубы чисел заканчивающиеся на четную цифру от 0 до {number}: ");
+
+for (double i = 1; i <= number; i++)
+{
+    double j = Math.Pow(i, 3);
+    double resultOst = j % 10;
+    if(resultOst % 2 == 0 && resultOst !=0)
+    Console.Write($"{j} ");
+}
