39e12786943027d3b6041d7f91d0a66697d85721
FreeK <andryushka_99@mail.ru>
2022-02-21 14:36:43 +0300

    Решение 36 задачи

---
+++ Task018/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задать массив, заполнить случайными положительными трёхзначными числами. Показать количество нечетных\четных чисел
+
+double[] arr = new double[10];
+int index = 0;
+double even = 0;
+double odd = 0;
+
+void FillArray()
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(100, 1000);
+        Console.Write($"{arr[i]} ");
+    }
+}
+
+FillArray();
+while (index < arr.Length)
+{
+    if (arr[index] % 2 == 0)
+    {
+        even++;
+    }
+    else
+    {
+        odd++;
+    }
+    index++;
+}
+
+Console.Write($"Нечетные {odd} ");
+Console.Write($"Четные {even} ");
