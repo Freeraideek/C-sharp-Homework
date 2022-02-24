f473f0d0c7ef468df98f6ff1637e2ca411112349
FreeK <andryushka_99@mail.ru>
2022-02-24 16:15:07 +0300

    Решение 44 задачи

---
+++ Task022/Program.cs
@@ -0,0 +1,30 @@
+﻿//Найти точку пересечения двух прямых заданных уравнением y = k1 * x + b1, y = k2 * x + b2, b1 k1 и b2 и k2 заданы
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project_44
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите  k1 ");
+            double k1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите  b1 ");
+            double b1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите  k2 ");
+            double k2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите  b2 ");
+            double b2 = Convert.ToDouble(Console.ReadLine());
+
+            if (k1 == k2)
+                Console.WriteLine("Нет точек пересечения");
+            else
+            Console.WriteLine($"Координаты точки пересечения x = {(b2 - b1) / (k1 - k2)}, y ={k1 * (b2 - b1) / (k1 - k2) + b1}");
+        }
+    }
+}
